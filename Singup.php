41630e1f6de33303c6417692b1cdb0938270c0f7
sahilkhatri68 <sahilkhatriss01@gmail.com>
2022-08-27 12:29:25 +0530

    new push

---
+++ Singup.php
@@ -0,0 +1,215 @@
+ <!-- <?php
+    $conn = mysqli_connect("localhost", "root", "", "harshaweb");
+
+    //login
+    // session_start();
+    //signup//
+    if (isset($_POST['signup'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $cpsw = $_POST['cpsw'];
+
+
+        if ($password == $cpsw) {
+            $insert = "INSERT INTO testing(name,email,password) VALUES('$name','$email','$password')";
+
+            $check = mysqli_query($conn, $insert);
+            header("location:http://projects.test/logincaptcha/Login.php");
+        } else {
+            echo "<div class='psw'>password does't match </div>";
+        }
+    }
+
+    ?>
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+     <style>
+     .maindiv {
+         padding: 60px;
+         background-image: url("./image/candle.jpg");
+
+     }
+
+     .insidediv {
+         width: 30%;
+     }
+
+     .btnone {
+         border: none;
+         background-color: #c08e4a;
+         color: white;
+         font-size: 50px;
+         cursor: pointer;
+         text-align: center;
+         width: 100%;
+     }
+
+     .empty {
+         height: 50px;
+     }
+
+     .contone {
+         background-color: black;
+         color: white;
+         font-size: 30px;
+         text-align: center;
+
+     }
+
+     .conttwo {
+         background-color: #c08e4a;
+         padding: 15px;
+     }
+
+     .inputt {
+         width: 97%;
+         height: 35px;
+         padding: 4px;
+     }
+
+     .bottomdiv {
+         width: 100%;
+     }
+
+     .sp {
+         color: black;
+     }
+
+     .lastCont {
+         width: 100%;
+         margin: auto;
+         text-align: center;
+         margin-top: 10px;
+         justify-content: center;
+         display: flex;
+         justify-content: center;
+         padding-bottom: 10px;
+     }
+
+     .itemsdiv {
+         display: flex;
+         justify-content: space-between;
+         margin-top: 10px;
+         width: 80%;
+         margin: auto;
+     }
+
+     @media(max-width:1056px) {
+         .insidediv {
+             width: 35%;
+         }
+     }
+
+     @media(max-width:900px) {
+         .insidediv {
+             width: 40%;
+         }
+     }
+
+     @media(max-width:750px) {
+         .insidediv {
+             width: 45%;
+         }
+
+     }
+
+     @media(max-width:700px) {
+         .itemsdiv {
+             width: 100%;
+         }
+
+     }
+
+     @media(max-width:650px) {
+         .insidediv {
+             width: 60%;
+         }
+     }
+
+     @media(max-width:500px) {
+         .insidediv {
+             width: 70%;
+         }
+
+         .maindiv {
+             padding: 50px;
+             margin: auto;
+
+         }
+     }
+
+     @media(max-width:450px) {
+         .insidediv {
+             width: 90%;
+             margin: auto;
+
+         }
+
+         .maindiv {
+             padding: 30px;
+             margin: auto;
+
+         }
+     }
+
+     @media(max-width:400px) {
+         .insidediv {
+             width: 100%;
+         }
+
+         .maindiv {
+             padding: 20px;
+             margin: auto;
+         }
+     }
+     </style>
+ </head>
+
+ <body>
+     <form class="maindiv" method="POST">
+         <div class="insidediv">
+             <button class="btnone" type="submit" name="signup">
+                 reC<span class="sp">r</span>ash
+             </button>
+             <div class="empty">
+             </div>
+             <div class="bottomcont">
+                 <div class="contone">Signup Here!</div>
+                 <div class="bottomdiv">
+                     <div class="conttwo">
+                         <input type="text" name="name" id="" placeholder="name" class="inputt" required>
+                     </div>
+                     <div class="conttwo">
+                         <input type="email" name="email" id="" placeholder="Email" class="inputt" required>
+                     </div>
+                     <div class="conttwo">
+                         <input type="password" name="password" id="" placeholder="password" class="inputt" required>
+
+                     </div>
+                     <div class="conttwo">
+                         <input type="password" name="cpsw" id="" placeholder="password" class="inputt" required>
+
+                     </div>
+                 </div>
+             </div>
+             <div class="lastCont">
+                 <div class="g-recaptcha" data-sitekey="6Lclj4ghAAAAAIQS_ljov07K8li9cY_7xwQlmN2K"></div>
+                 <br />
+
+             </div>
+             <div class="itemsdiv">
+                 <!-- <div class="item"> <input type="checkbox" name="" id="">Remember </div> -->
+             </div>
+         </div>
+     </form>
+ </body>
+
+ </html> -->
